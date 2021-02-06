c9eeeac2668b217f61acd6d2aef81f84e4f78552
yaredg <yaredgidey@gmaile.com>
2021-02-05 19:50:49 -0500

    Persistent based remember me implemented.

--- src/main/java/com/yaredgidey/studentdatauiapp/config/WebConfig.java
+++ src/main/java/com/yaredgidey/studentdatauiapp/config/WebConfig.java
@@ -1,16 +1,31 @@
 package com.yaredgidey.studentdatauiapp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebConfig  extends WebSecurityConfigurerAdapter {
 
 
+    @Autowired
+    DataSource dataSource;
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl tokenRepositoryImpl = new JdbcTokenRepositoryImpl();
+        tokenRepositoryImpl.setDataSource(dataSource);
+        return tokenRepositoryImpl;
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -23,9 +38,13 @@ public class WebConfig  extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/students", true)
                 .permitAll()
                 .and()
+                .rememberMe().rememberMeParameter("remember-me")
+                .tokenRepository(persistentTokenRepository())
+                .tokenValiditySeconds(86400)
+                .and()
                 .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/home")
-                .invalidateHttpSession(true)        // set invalidation state when logout
+                .invalidateHttpSession(true)   // set invalidation state when logout
                 .deleteCookies("JSESSIONID")
                 .and()
                 .exceptionHandling()
